ce040605767d1b17a455da53fa7dcb672aed8392
heedaelee <adguy72@gmail.com>
2024-09-26 15:31:42 +0900

    chore: 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className='flex items-center justify-center h-screen'>
       <div>하이</div>
     </div>
   );
