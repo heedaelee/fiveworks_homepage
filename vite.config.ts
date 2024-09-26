c2d39877fc8834ed36358416dae4200c34daf0ac
heedaelee <adguy72@gmail.com>
2024-09-26 15:31:26 +0900

    chore: vite 초기 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-macros'],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src', // '@'를 사용하면 src 폴더를 가리키도록 설정
+    },
+  },
+  server: {
+    port: 7090,
+  },
+});
