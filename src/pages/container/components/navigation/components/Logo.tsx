c018496106a91459da37ea86b5efb8b09b86556c
heedaelee <adguy72@gmail.com>
2024-09-27 18:35:02 +0900

    fix: logo 추가

---
+++ src/pages/container/components/navigation/components/Logo.tsx
@@ -0,0 +1,30 @@
+import {Link, useNavigate} from 'react-router-dom';
+import logoDefault from '@/assets/img/logo-default.png';
+import logoWhite from '@/assets/img/logo-white.png';
+import useIsHomepage from '@/hooks/useIsHomePage';
+
+const Logo = ({isOpen}: {isOpen: boolean}) => {
+  const isHomePage = useIsHomepage();
+  const navigate = useNavigate();
+  const getLogoImagePath = () => {
+    if (isHomePage) {
+      return isOpen ? logoDefault : logoWhite;
+    }
+    return logoDefault;
+  };
+
+  return (
+    <div className='bg-yellow-200 w-[15%] flex flex-row justify-center p-[20px] h-[125px]'>
+      <Link to='/'>
+        <img
+          src={getLogoImagePath()}
+          alt='logo'
+          className='h-[70px] object-cover opacity-transition fade-in'
+          onClick={() => navigate('/')}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
