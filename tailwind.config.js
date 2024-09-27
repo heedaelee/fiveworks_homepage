8cf1e12079dfbf53e86c5ba619f3fbfd385c920a
heedaelee <adguy72@gmail.com>
2024-09-27 12:57:09 +0900

    fix: tailwindcss container 클래스 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./**/@material-tailwind/**/*.{html,js,ts,jsx,tsx,mdx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './**/@material-tailwind/**/*.{html,js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({addComponents}) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1280px',
+          },
+          '@screen xl': {
+            maxWidth: '1420px',
+          },
+        },
+      });
+    },
+  ],
 };
