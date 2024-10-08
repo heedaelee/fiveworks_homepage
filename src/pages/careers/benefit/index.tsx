73da586f9789c701be26eac704af8147804f00f2
heedaelee <adguy72@gmail.com>
2024-10-08 10:24:06 +0900

    fix: 복지 후생 아이콘 추가

--- src/pages/careers/benefit/index.tsx
+++ src/pages/careers/benefit/index.tsx
@@ -1,31 +1,32 @@
 import {Card, CardContent} from '@/components/ui/card';
+import {TreePalm, HandCoins, Clock, Donut, PartyPopper, Cake, LibraryBig, Gift} from 'lucide-react';
 
-const Icon = ({d, label}: {d: string; label: string}) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    viewBox='0 0 24 24'
-    fill='none'
-    stroke='currentColor'
-    strokeWidth='2'
-    strokeLinecap='round'
-    strokeLinejoin='round'
-    className='w-32 h-32 mb-4 text-primary mx-auto'
-    aria-label={label}>
-    <path d={d} />
-  </svg>
-);
+// const Icon = ({d, label}: {d: string; label: string}) => (
+//   <svg
+//     xmlns='http://www.w3.org/2000/svg'
+//     viewBox='0 0 24 24'
+//     fill='none'
+//     stroke='currentColor'
+//     strokeWidth='2'
+//     strokeLinecap='round'
+//     strokeLinejoin='round'
+//     className='w-32 h-32 mb-4 text-primary mx-auto'
+//     aria-label={label}>
+//     <path d={d} />
+//   </svg>
+// );
 
 const Benefits = () => {
   const benefitItems = [
-    {icon: 'M18 8h1a4 4 0 0 1 0 8h-1', text: '연차 및 리프레시 휴가제공'},
-    {icon: 'M12 1v22M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6', text: '연차 수당 지급'},
-    {icon: 'M12 6v6l4 2', text: '9시~10시 출근제도'},
-    {icon: 'M18 8h1a4 4 0 0 1 0 8h-1', text: '간식제공'},
-    {icon: 'M20 12v7a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7', text: '경조사비 지급'},
-    {icon: 'M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2', text: '생일자 오후 반차'},
-    {icon: 'M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z', text: '업무관련 교육 및 도서구입'},
+    {Icon: TreePalm, text: '연차 및 리프레시 휴가제공'},
+    {Icon: HandCoins, text: '연차 수당 지급'},
+    {Icon: Clock, text: '9시~10시 출근제도'},
+    {Icon: Donut, text: '간식제공'},
+    {Icon: PartyPopper, text: '경조사비 지급'},
+    {Icon: Cake, text: '생일자 오후 반차'},
+    {Icon: LibraryBig, text: '업무관련 교육 및 도서구입'},
     {
-      icon: 'M8 2v4M16 2v4M3 10h18M21 8v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z',
+      Icon: Gift,
       text: '명절 및 창립기념일 선물',
     },
   ];
@@ -37,10 +38,11 @@ const Benefits = () => {
 
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6'>
         {benefitItems.map((item, index) => (
-          <Card key={index} className='flex flex-col items-center justify-center p-4'>
-            <CardContent className='text-center'>
-              <Icon d={item.icon} label={item.text} />
-              <p className='text-sm'>{item.text}</p>
+          <Card key={index} className='flex flex-col items-center justify-center'>
+            <CardContent className='text-center py-12'>
+              {/* <Icon d={item.icon} label={item.text} /> */}
+              <item.Icon className='w-16 h-16 mx-auto mb-4 text-primary' />
+              <p className='text-lg'>{item.text}</p>
             </CardContent>
           </Card>
         ))}
