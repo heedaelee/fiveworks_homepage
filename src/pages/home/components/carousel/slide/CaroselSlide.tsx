c79c33fbef2c5409448f6387df961b94f20cde57
heedaelee <adguy72@gmail.com>
2024-10-21 15:57:29 +0900

    fix: 캐러셀 글자 가로선 맞춤

--- src/pages/home/components/carousel/slide/CaroselSlide.tsx
+++ src/pages/home/components/carousel/slide/CaroselSlide.tsx
@@ -6,7 +6,7 @@ const CarouselSlide = ({imageUrl, title, description, currentIndex}: CarouselSli
     <div className='embla__slide relative flex-[0_0_100%]'>
       <img src={imageUrl} className='h-full w-full object-cover absolute' />
       <div className='absolute inset-0 bg-black opacity-50' />
-      <div className='carousel-caption absolute top-[48%] left-[38%] transform -translate-x-1/2 -translate-y-1/2 text-white z-50'>
+      <div className='carousel-caption absolute top-[48%] left-[18%]  -translate-y-1/2 text-white z-50'>
         <h5 className='font-[600] text-[40px] sm:text-[65px]'>{title}</h5>
         <p className='mt-2 sm:mt-0 mb-1'>{description}</p>
         <Rocket currentIndex={currentIndex} />
