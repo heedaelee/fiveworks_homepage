cfb90c53a73635462aedde3996c81fbdadabf714
heedaelee <adguy72@gmail.com>
2024-10-21 17:09:39 +0900

    fix: 자료 변경에 따른 그룹 크기 재조정

--- src/pages/intro/group/components/DesktopChart/index.tsx
+++ src/pages/intro/group/components/DesktopChart/index.tsx
@@ -7,14 +7,15 @@ const DesktopChart = () => {
   const departments: Department[] = getDepartments(t);
 
   return (
-    <div className='container mx-auto px-4 py-16 pb-40'>
+    <div className='container mx-auto px-4 '>
       <div className='w-full h-[900px] overflow-hidden'>
         <svg
-          width={1600}
-          height={1000}
-          viewBox='45 340 1650 900'
+          // width={600}
+          // height={1600}
+          viewBox='0 300 900 900'
           className='w-full h-full'
-          style={{transform: `scale(1.5)`, transformOrigin: 'top left'}}>
+          style={{transform: `scale(1.5)`, transformOrigin: 'top left'}}
+          >
           <g transform='translate(50, 600)'>{renderDepartmentTree(departments[0], 0, 0, 0)}</g>
         </svg>
       </div>
