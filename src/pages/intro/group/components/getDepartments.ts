63219d2958848309c79fa2dc68a30172400d5a0d
heedaelee <adguy72@gmail.com>
2024-10-21 17:09:01 +0900

    fix: intro-group 마지막 뎁스 삭제

--- src/pages/intro/group/components/getDepartments.ts
+++ src/pages/intro/group/components/getDepartments.ts
@@ -10,36 +10,19 @@ export const getDepartments = (t: TFunction): Department[] => [
     name: 'CEO',
     children: [
       {
-        name: t('intro.group.items.vicePresident'),
-        children: [
-          {
-            name: t('intro.group.items.dataSolution.title'),
-            children: [
-              t('intro.group.items.dataSolution.items.solutionA'),
-              t('intro.group.items.dataSolution.items.solutionB'),
-              t('intro.group.items.dataSolution.items.solutionC'),
-              t('intro.group.items.dataSolution.items.solutionD'),
-            ],
-          },
-          {
-            name: t('intro.group.items.management.title'),
-            children: [
-              t('intro.group.items.management.items.management'),
-              t('intro.group.items.management.items.strategy'),
-            ],
-          },
-          {
-            name: t('intro.group.items.ai.title'),
-            children: [t('intro.group.items.ai.items.ai')],
-          },
-          {
-            name: t('intro.group.items.public.title'),
-            children: [
-              t('intro.group.items.public.items.bigData'),
-              t('intro.group.items.public.items.publicBusiness'),
-            ],
-          },
-        ],
+        name: t('intro.group.items.dataSolution.title'),
+      },
+      {
+        name: t('intro.group.items.bigdata.title'),
+      },
+      {
+        name: t('intro.group.items.management.title'),
+      },
+      {
+        name: t('intro.group.items.ai.title'),
+      },
+      {
+        name: t('intro.group.items.public.title'),
       },
     ],
   },
