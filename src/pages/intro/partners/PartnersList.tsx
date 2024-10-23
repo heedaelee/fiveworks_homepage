78bacbf791f59e066a10541db12449681f73ebbb
heedaelee <adguy72@gmail.com>
2024-10-23 16:22:34 +0900

    fix: 로고 수정

--- src/pages/intro/partners/PartnersList.tsx
+++ src/pages/intro/partners/PartnersList.tsx
@@ -13,13 +13,13 @@ const PartenersList = () => {
     <div>
       {partnerImages.map((images, index) => {
         return (
-          <section className='mb-16'>
-            <div className='w-fit bg-[#eeecec90] rounded-3xl p-5 '>
+          <section className='mb-20'>
+            <div className='w-fit bg-[#eeecec90] rounded-2xl p-5 shadow-md mb-8'>
               <h2 className='text-3xl font-semibold text-[#6c6b6b]'>
                 {index === 0 ? 'Company' : index === 1 ? 'Education' : 'Government'}
               </h2>
             </div>
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 '>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  '>
               {images.map((image, index2) => (
                 <motion.div
                   key={index2}
